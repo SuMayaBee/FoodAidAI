fdaf0f34d1b9837a97dbe35e7e7d3059eb8915ce
ARAFAT <108474267+arafatDU@users.noreply.github.com>
2024-09-10 01:15:19 +0600

    Spring AI with Open AI done

--- Backend/src/main/java/com/redy/blogbackend/services/ChatService.java
+++ Backend/src/main/java/com/redy/blogbackend/services/ChatService.java
@@ -1,2 +1,61 @@
-package com.redy.blogbackend.services;public class ChatService {
+package com.redy.blogbackend.services;
+
+
+import org.springframework.ai.chat.messages.Media;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
+
+@Service
+public class ChatService {
+
+    @Autowired
+    ChatModel chatClient;
+
+
+
+    public ChatResponse gentChatResponse(String category, String year){
+
+        PromptTemplate promptTemplate = new PromptTemplate(
+                """
+                Please provide me best book for the given {category} and the {year}.
+                Please do provide a summary of the book as well, the information should be 
+                limited and not much in depth. Please provide the details in the JSON format
+                containing this information : category, book, year, review, author, summary
+                """
+        );
+
+        promptTemplate.add("category", category);
+        promptTemplate.add("year", year);
+
+        Prompt prompt = promptTemplate.create();
+
+        return chatClient.call(prompt);
+
+    }
+
+
+    public String getImageChatReader(String query, String imageBase64) {
+        // Decode the base64 image to byte array
+
+        byte[] imageData = Base64.getDecoder().decode(imageBase64);
+
+        UserMessage userMessage = new UserMessage(query,
+                List.of(new Media(MimeTypeUtils.IMAGE_PNG, imageData)));
+
+        ChatResponse response = chatClient.call(new Prompt(userMessage));
+        return response.getResult().getOutput().getContent();
+    }
+
 }
